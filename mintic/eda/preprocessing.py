3cd5bf303becf05bcc8cc2ed5c2c9835c31f17aa
techcanto <antonioclash819@gmail.com>
2026-09-01 09:45:08 -0600

    Funcion impute_missing

--- mintic/eda/preprocessing.py
+++ mintic/eda/preprocessing.py
@@ -1,5 +1,28 @@
 # Código para poner las funciones de preprocesamiento
 
-def impute_missing(data, strategy="mean", columns=None):
-	pass
+import pandas as pd
 
+# data:Dataframe de pandas
+# columns: lista de nombres de las columnas a revisar (pasar solo columnas numericas)
+
+def impute_missing(data, strategy="mean", columns=None):
+        booleanosData = [_ for _ in data.isna().any()]
+        if any(booleanosData):
+                #columnas = list(data.columns)
+                resultado = data.copy()
+
+                if strategy == "mean":
+                        for columna in columns:
+                                resultado[columna] = resultado[columna].fillna(resultado[columna].mean())
+                                #print("Se aplicó la media a las columnas faltantes")
+                elif strategy == "median":
+                        for columna in columns:
+                                resultado[columna] = resultado[columna].fillna(resultado[columna].median())
+                                #print("Se aplicó la mediana a las columnas faltantes")
+                elif strategy == "mode":
+                        for columna in columns:
+                                resultado[columna] = resultado[columna].fillna(resultado[columna].mode()[0])
+                                #print("Se aplicó la moda a las columnas faltantes")
+                return resultado
+        else:
+                print("No hay valores NAN")
